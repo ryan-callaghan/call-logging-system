64d4f6376e006c1485b23b4d6d6ecec10b221d2d
Ryan <ryancallaghan.p@gmail.com>
2025-09-14 20:29:00 -0400

    restructured event-generator directory to telco-event-generator, added maven dependencies (pom.xml) and application configuration (application.yml) as well as designed avro schemas (call event, session event, network event)

---
+++ telco-event-generator/src/main/java/com/telco/events/TelcoEventGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.telco.events;
+
+public class TelcoEventGeneratorApplication {
+    
+}
+
+@SpringBootApplication
+public class TelcoEventGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelcoEventGeneratorApplication.class, args);
+    }
+}
